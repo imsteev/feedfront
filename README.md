457a7357980f50b324a234df5e7e583bdd5605e6
Stephen Chung <spchung95@gmail.com>
2024-01-23 23:03:19 -0500

    learnings

--- README.md
+++ README.md
@@ -13,3 +13,24 @@ bun run index.ts
 ```
 
 This project was created using `bun init` in bun v1.0.13. [Bun](https://bun.sh) is a fast all-in-one JavaScript runtime.
+
+## Learnings (last update 1/23/24)
+    1. There are situations where you need to handle behavior based on
+whether or not the request is an HTMX request. For example, redirections
+through HTMX requires "HX-Redirect". Things could get weird if HTMX redirect-
+and native redirect- headers are included at the same time.
+    ```
+    <form action="/login" method="post">
+    <form hx-post"/login">
+    ```
+
+    2. From docs(https://htmx.org/docs/#history):
+  >  NOTE: If you push a URL into the history, you must be able to navigate to
+  that URL and get a full page back! A user could copy and paste the URL into
+  an email, or new tab. Additionally, htmx will need the entire page when
+  restoring history if the page is not in the history cache.
+
+    3. "no-cache does not mean "don't cache". no-cache allows caches to
+  store a response but requires them to revalidate it before reuse. If the
+  sense of "don't cache" that you want is actually "don't store", then
+  no-store is the directive to use."
--- index.ts
+++ index.ts
@@ -18,28 +18,3 @@ const server = Bun.serve({
 });
 
 console.log(`listening at ${server.url}`);
-
-/** LEARNINGS
- *
- * 1. There are situations where you need to handle behavior based on
- * whether or not the request is an HTMX request. For example, redirections
- * through HTMX requires "HX-Redirect". Things could get weird if HTMX redirect-
- * and native redirect- headers are included at the same time.
- *
- * <form action="/login" method="post">
- *
- * <form hx-post"/login">
- *
- * 2. From docs(https://htmx.org/docs/#history):
- *   NOTE: If you push a URL into the history, you must be able to navigate to
- *   that URL and get a full page back! A user could copy and paste the URL into
- *   an email, or new tab. Additionally, htmx will need the entire page when
- *   restoring history if the page is not in the history cache.
- *
- * 3. This is kind of hilarious, about "Cache-Control: no-cache"
- *
- *   Note that no-cache does not mean "don't cache". no-cache allows caches to
- *   store a response but requires them to revalidate it before reuse. If the
- *   sense of "don't cache" that you want is actually "don't store", then
- *   no-store is the directive to use.
- */
